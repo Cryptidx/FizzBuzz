cfca49c5780a3551e52b42d3aa1c2dcfdeb26d2a
hannahomeiza <omeiza81@gmail.com>
2023-09-13 12:06:37 -0400

    extracted helper method and made new Main method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,42 +5,42 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
+        doFizzBuzz(5);
+    }
+
+    public static void doFizzBuzz(int n) {
         int i = 1;
 
         while (i < 100) {
-            i = doFizzBuzz(i);
-        }
-    }
+            boolean divisibleBy3 = i % 3 == 0;     // this boolean constantly checks this for different values of i
+            boolean divisibleBy5 = i % 5 == 0;
 
-    private static int doFizzBuzz(int i) {
-        boolean divisibleBy3 = i % 3 == 0;     // this boolean constantly checks this for different values of i
-        boolean divisibleBy5 = i % 5 == 0;
+            // Find out which numbers divide i.
 
-        // Find out which numbers divide i.
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
 
-        // Print our appropriate result.
-        if (divisibleBy3 && divisibleBy5) {
-
-            System.out.println("Fizz Buzz");
+                System.out.println("Fizz Buzz");
 
 
-        } else if (divisibleBy3) {
+            } else if (divisibleBy3) {
 
-            System.out.println("Fizz");
-
-
-        } else if (divisibleBy5) {
-
-            System.out.println("Buzz");
+                System.out.println("Fizz");
 
 
-        } else {
+            } else if (divisibleBy5) {
 
-            System.out.println(i);
-            
+                System.out.println("Buzz");
 
-        }
-        i++;
-        return i;
+
+            } else {
+
+                System.out.println(i);
+
+
+            }
+            i++; }
     }
 }
+
+   
